87553550b4d0ab040ff494700e9dc672853716a3
YourtionGuo <yourtion@gmail.com>
2017-01-11 22:30:26 +0800

    init project

---
+++ WebSocket/WebSocket.h
@@ -0,0 +1,19 @@
+//
+//  WebSocket.h
+//  WebSocket
+//
+//  Created by YourtionGuo on 11/01/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebSocket.
+FOUNDATION_EXPORT double WebSocketVersionNumber;
+
+//! Project version string for WebSocket.
+FOUNDATION_EXPORT const unsigned char WebSocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebSocket/PublicHeader.h>
+
+
